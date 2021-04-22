20a4425374e2e7219f0351bd566ff6e70520a3d5
vairiskovels <47304563+vairiskovels@users.noreply.github.com>
2021-04-22 21:19:04 +0300

    Update application.py

--- application.py
+++ application.py
@@ -32,9 +32,9 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
 #app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use PostgreSQL database
