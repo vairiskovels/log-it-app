d2195e2d16ed21bb983c391a6e961f0060e30d87
vairiskovels <47304563+vairiskovels@users.noreply.github.com>
2021-04-22 21:13:52 +0300

    Update application.py

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ def after_request(response):
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -120,6 +120,7 @@ def login():
         else:
             # Remember which user has logged in
             session["user_id"] = rows[0]["id"]
+            session.permanent = True
 
             # Redirect user to home page
             return redirect("/")
